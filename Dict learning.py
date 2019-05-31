f4694e60d391739c8915fb21bf378ae5774dc36c
Max <max.gtn@yandex.ru>
2019-05-31 14:26:50 +0300

    dict v3

--- Dict learning.py
+++ Dict learning.py
@@ -1,7 +1,26 @@
-# DON'T TOUCH PLEASE!
-donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5, stan=150.0, lisa=50.25, harrison=10.0)
-# DON'T TOUCH PLEASE!
-for v in donations.values():
-    print(v)
+# # # DON'T TOUCH PLEASE!
+# # donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5, stan=150.0, lisa=50.25, harrison=10.0)
+# # # DON'T TOUCH PLEASE!
+# # for v in donations.values():
+# #     print(v)
+# #
+# # # Use a loop torespo add together all the donations and store the resulting number in a variable called total_donations
+#
+# # This code picks a random food item:
+# from random import choice #DON'T CHANGE!
+# food = choice(["cheese pizza", "quiche","morning bun","gummy bear","tea cake"]) #DON'T CHANGE!
+#
+# #DON'T CHANGE THIS DICTIONARY EITHER!
+# bakery_stock = {
+#     "almond croissant" : 12,
+#     "toffee cookie": 3,
+#     "morning bun": 1,
+#     "chocolate chunk cookie": 9,
+#     "tea cake": 25
+# }
+# if food in bakery_stock:
+#     print(f"{bakery_stock.get(food)} left of {food}")
+# else:
+#     print("We don't make that")
+# # YOUR CODE GOES BELOW:
 
-# Use a loop to add together all the donations and store the resulting number in a variable called total_donations
